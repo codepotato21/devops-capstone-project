8c1008608fe0139ccf07bc28fab291d84252042d
codepotato21 <asokans443@gmail.com>
2024-11-06 05:18:47 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -190,7 +190,7 @@ class TestAccountService(TestCase):
         response = self.client.delete(f"{BASE_URL}")
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
     
-    def test_cors_security(self):
+    def test_security_headers(self):
         """ It should return security headers"""
         response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -202,3 +202,9 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+    def test_flask_cors(self):
+        """ It should return a CORS header """
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
